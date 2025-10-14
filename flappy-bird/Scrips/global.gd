90b662abf764d0fea681512f533dd9c08bd00689
Nahomy Olachea <miriamnaho_25@alu.uabcs.mx>
2025-10-14 14:09:36 -0700

    Uní mis cambios xd

---
+++ flappy-bird/Scrips/global.gd
@@ -0,0 +1,18 @@
+extends Node
+
+signal on_Increment_score
+
+var score := 0
+var is_start := false
+
+# func game_start():
+	# is_start = true
+	
+
+func Increment_score():
+	score += 1
+	emit_signal("on_Increment_score")
+	
+func game_over():
+	get_tree().paused = true
+	# acá el juego va a quedar pausado
