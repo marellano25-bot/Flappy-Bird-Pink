6cc8388c2d24a6cb02f1d08c9112eafcbc77cd62
Nahomy Olachea <miriamnaho_25@alu.uabcs.mx>
2025-11-20 18:43:24 -0700

    Números de pixel

--- Scrips/canvas_layer.gd
+++ Scrips/canvas_layer.gd
@@ -1,8 +1,38 @@
 extends CanvasLayer
 
+var numeros := []
+
 func _ready() -> void:
 	Global.connect("on_Increment_score", change_score)
 	Global.connect("on_game_start", _on_game_start)
+	#para los números
+	for i in range(10):
+		var coso = load("res://art/%d.png" % i)
+		numeros.append(coso)
+		#vector2, es decir que son dos valores juntos, como tamaño,escala, posicion, etc
+		
+		 
+		
+		$num_1.scale = Vector2(0.10, 0.10)
+		$num_2.scale = Vector2(0.10, 0.10)
+	
+	$num_1.hide()
+	$num_2.hide()
+
+func actualizar_numeros(score: int) -> void:
+	var texto := str(score)
+	
+	if texto.length() == 1:
+		$num_1.hide()
+		$num_2.show()
+		$num_2.texture = numeros[int(texto[0])]
+		
+	elif texto.length() == 2:
+		$num_1.show()
+		$num_2.show()
+		$num_1.texture = numeros[int(texto[0])]
+		$num_2.texture = numeros[int(texto[1])]
+	
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		if Global.is_start == false:
@@ -14,8 +44,8 @@ func _process(delta: float) -> void:
 	pass
 func change_score():
 	$LabelScore.text = str(Global.score)
-	
-	
+	actualizar_numeros(Global.score)
+		
 func _on_game_start() -> void:
 	start_game()
 	
