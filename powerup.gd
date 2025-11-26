8b6784f582b4b6a0b0d17b84323e0150f99c2f9e
Melissa Arellano <marellano_25@alu.uabcs.mx>
2025-11-26 09:14:46 -0700

    Sonido del power up

--- powerup.gd
+++ powerup.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var pu: AudioStreamPlayer2D = $powerup
 
 @export var tiempo_reaparicion := 10.0
 @export var velocidad := 150
@@ -41,6 +42,8 @@ func _on_body_entered(body: Node2D) -> void:
 			coold = true
 			sprite.hide()
 			collision.disabled = true
+			pu.play()
+		
 			if body.has_method("activar_inmunidad"):
 				body.activar_inmunidad(tiempo_inmunidad)
 			if body.has_method("use_power_up"):
